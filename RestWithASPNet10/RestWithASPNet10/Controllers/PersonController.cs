78af5da82651e2d7801710c6532804a0d28cafb1
Isaac Lima <isaac.lima25@aluno.ce.gov.br>
2026-02-23 18:45:29 -0300

    :sparkles: feat: adicionado função de log nos controllers da API

--- RestWithASPNet10/RestWithASPNet10/Controllers/PersonController.cs
+++ RestWithASPNet10/RestWithASPNet10/Controllers/PersonController.cs
@@ -10,25 +10,32 @@ namespace RestWithASPNet10.Controllers
     public class PersonController : ControllerBase
     {
         private readonly IPersonServices _personServices;
+        private readonly ILogger<PersonController> _logger;
 
-        public PersonController(IPersonServices personServices)
+        public PersonController(IPersonServices personServices, ILogger<PersonController> logger)
         {
             _personServices = personServices;
+            _logger = logger;
         }
 
         [HttpGet]
         public IActionResult Get()
         {
+            _logger.LogInformation("Getting all people");
+
             return Ok(_personServices.FindAll());
         }
 
         [HttpGet("{id}")]
         public IActionResult Get(long id)
         {
+            _logger.LogInformation("Getting person with id {Id}", id);
+
             var person = _personServices.FindById(id);
 
             if(person == null)
             {
+                _logger.LogWarning("Person with id {Id} not found", id);
                 return NotFound();
             }
             else
@@ -40,7 +47,14 @@ namespace RestWithASPNet10.Controllers
         [HttpPost]
         public IActionResult Post([FromBody] Person person)
         {
+            _logger.LogInformation("Creating a new person: {firstName}", person.FirstName);
             var createdPerson = _personServices.Create(person);
+
+            if(createdPerson == null)
+            {
+                _logger.LogError("Failed to create person: {firstName}", person.FirstName);
+                return BadRequest();
+            }
 
             return Ok(person);
         }
@@ -48,7 +62,17 @@ namespace RestWithASPNet10.Controllers
         [HttpPut]
         public IActionResult Put(Person person)
         {
+            _logger.LogInformation("Updating person with id {Id}", person.Id);
+             
             var createdPerson = _personServices.Update(person);
+
+            if(createdPerson == null)
+            {
+                _logger.LogError("Failed to update person with id {Id}", person.Id);
+                return BadRequest();
+            }
+
+            _logger.LogDebug("Person with id {Id} updated successfully", person.Id);
 
             return Ok(person);
         }
@@ -56,8 +80,18 @@ namespace RestWithASPNet10.Controllers
         [HttpDelete("{id}")]
         public IActionResult Delete(long id)
         {
+            var person = _personServices.FindById(id);
+
+            if (person == null)
+            {
+                _logger.LogError("Failed to delete person with id {Id}", id);
+                return NotFound();
+            }
+
+            _logger.LogInformation("Deleting person with id {Id}", id);
             _personServices.Delete(id);
 
+            _logger.LogDebug("Person with id {Id} deleted successfully", id);
             return NoContent();
         }
     }
--- RestWithASPNet10/RestWithASPNet10/obj/Debug/net10.0/RestWithASPNet10.AssemblyInfo.cs
+++ RestWithASPNet10/RestWithASPNet10/obj/Debug/net10.0/RestWithASPNet10.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestWithASPNet10")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cefa28b3d05785d81f5778b491698684ef68ae27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a780206c5f99055a0f11840b3fc2c0319bb4e68c")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestWithASPNet10")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestWithASPNet10")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

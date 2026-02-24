e4fecc131d1152a4bff49d62e94df7407d37a16c
Isaac Lima <isaac.lima25@aluno.ce.gov.br>
2026-02-24 09:17:09 -0300

    :sparkles: feat: adicionado primeira migration

--- RestWithASPNet10/RestWithASPNet10/obj/Debug/net10.0/RestWithASPNet10.AssemblyInfo.cs
+++ RestWithASPNet10/RestWithASPNet10/obj/Debug/net10.0/RestWithASPNet10.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestWithASPNet10")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba36b8708bb0ad99a77736082d184ca8dd2f46ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58f9442b7aacc6289adae794bcf06b389568e7c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestWithASPNet10")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestWithASPNet10")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
